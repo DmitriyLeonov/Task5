982005f8a8599b45130caf910e3feb93c4a5a2b9
DmitriyLeonov <dleonov701@gmail.com>
2023-03-18 12:30:21 +0300

    Add project files.

---
+++ Task5/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task5.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):
+            base(options)
+        {
+            Database.EnsureCreatedAsync();
+        }
+    }
+}
