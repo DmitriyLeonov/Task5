a252b8d832b375fe21fe2d756286801bd853798d
DmitriyLeonov <dleonov701@gmail.com>
2023-03-22 22:15:13 +0300

    input errors generator added

--- Task5/Controllers/HomeController.cs
+++ Task5/Controllers/HomeController.cs
@@ -5,11 +5,17 @@ using Microsoft.EntityFrameworkCore;
 using Task5.Data;
 using Task5.Models;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
 
 namespace Task5.Controllers
 {
     public class HomeController : Controller
     {
+        public static string spanishAlphabet = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789";
+        public static string italianAlphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzÀàÈèÉéÌìÒòÙù0123456789";
+        public static string germanAlphabet = "AÄBCDDEFGHIİJJKLMNOÖPQRSTUÜVWXYZaäbcddefghii̇jjklmnoöpqrstuüvwxyz0123456789";
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
         public static List<User> users = new List<User>();
@@ -84,16 +90,129 @@ namespace Task5.Controllers
                 user.PhoneNumber = PhoneNumbers[random.Next(0, PhoneNumbers.Count())].Number;
                 user.FullName = FirstNames[random.Next(0, FirstNames.Count())].Name
                                 + " " + LastNames[random.Next(0, LastNames.Count())].SecondName;
+                if (errorsCount > 0)
+                {
+                    _logger.LogInformation("error count{errorCount}:", errorsCount);
+                    GenerateErrors(user);
+                }
                 users.Add(user);
-            }
-
-            if (errorsCount > 0)
-            {
-                _logger.LogInformation("error count{errorCount}",errorsCount);
             }
             count = 10;
             currentPage++;
             return users;
+        }
+
+        public void GenerateErrors(User user)
+        {
+            var errors = Math.Truncate(errorsCount + new Random().NextDouble());
+            Random rnd = new Random();
+            var num = rnd.NextDouble();
+            if (num <= 0.33)
+            {
+                AddSymbolError(errors, user);
+            }
+            else if(num <= 0.66)
+            {
+                RemoveSymbolError(errors, user);
+            }
+            else
+            {
+                ChangeSymbolsError(errors, user);
+            }
+        }
+
+        private void ChangeSymbolsError(double errors, User user)
+        {
+            Random rnd = new Random();
+            int pos;
+            char tmp, tmp1;
+            for (int i = 0; i < errors; i++)
+            {
+                pos = rnd.Next(0, user.UserId.Length - 2);
+                tmp = user.UserId[pos];
+                tmp1 = user.UserId[pos + 1];
+                user.UserId = user.UserId.Remove(pos, 2);
+                user.UserId = user.UserId.Insert(pos, tmp1.ToString());
+                user.UserId = user.UserId.Insert(pos + 1, tmp.ToString());
+                pos = rnd.Next(0, user.Address.Length - 2);
+                tmp = user.Address[pos];
+                tmp1 = user.Address[pos + 1];
+                user.Address = user.Address.Remove(pos, 2);
+                user.Address = user.Address.Insert(pos, tmp1.ToString());
+                user.Address = user.Address.Insert(pos + 1, tmp.ToString());
+                pos = rnd.Next(0, user.PhoneNumber.Length - 2);
+                tmp = user.PhoneNumber[pos];
+                tmp1 = user.PhoneNumber[pos + 1];
+                user.PhoneNumber = user.PhoneNumber.Remove(pos, 2);
+                user.PhoneNumber = user.PhoneNumber.Insert(pos, tmp1.ToString());
+                user.PhoneNumber = user.PhoneNumber.Insert(pos + 1, tmp.ToString());
+                pos = rnd.Next(0, user.FullName.Length - 2);
+                tmp = user.FullName[pos];
+                tmp1 = user.FullName[pos + 1];
+                user.FullName = user.FullName.Remove(pos, 2);
+                user.FullName = user.FullName.Insert(pos, tmp1.ToString());
+                user.FullName = user.FullName.Insert(pos + 1, tmp.ToString());
+                }
+        }
+
+        private void RemoveSymbolError(double errors, User user)
+        {
+            try
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < errors; i++)
+                {
+                    if (user.UserId.Length <= 5 || user.Address.Length <= 5 || user.PhoneNumber.Length <= 5 ||
+                        user.FullName.Length <= 5)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    else
+                    {
+                        user.UserId = user.UserId.Remove(rnd.Next(0, rnd.Next(0, user.UserId.Length - 1)), 1);
+                        user.Address = user.Address.Remove(rnd.Next(0, rnd.Next(0, user.Address.Length - 1)), 1);
+                        user.PhoneNumber = user.PhoneNumber.Remove(rnd.Next(0, rnd.Next(0, user.PhoneNumber.Length - 1)), 1);
+                        user.FullName = user.FullName.Remove(rnd.Next(0, rnd.Next(0, user.FullName.Length - 1)), 1);
+                    }
+                       
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                AddSymbolError(errors, user);
+            }
+        }
+
+        private void AddSymbolError(double errors, User user)
+        {
+            Random rnd = new Random();
+            string symbols;
+            if (locale.Equals("it"))
+            {
+                symbols = italianAlphabet;
+            } else if (locale.Equals("es"))
+            {
+                symbols = spanishAlphabet;
+            }
+            else
+            {
+                symbols = germanAlphabet;
+            }
+            for (int i = 0; i < errors; i++)
+            {
+                if (user.UserId.Length >= 40 || user.Address.Length >= 150 || user.PhoneNumber.Length >= 20 ||
+                    user.FullName.Length >= 40)
+                {
+
+                }
+                else
+                {
+                    user.UserId = user.UserId.Insert(rnd.Next(0, user.UserId.Length - 1), symbols[rnd.Next(0, symbols.Length - 1)].ToString());
+                    user.Address = user.Address.Insert(rnd.Next(0, user.Address.Length - 1), symbols[rnd.Next(0, symbols.Length - 1)].ToString());
+                    user.PhoneNumber = user.PhoneNumber.Insert(rnd.Next(0, user.PhoneNumber.Length - 1), symbols[rnd.Next(0, symbols.Length - 1)].ToString());
+                    user.FullName = user.FullName.Insert(rnd.Next(0, user.FullName.Length - 1), symbols[rnd.Next(0, symbols.Length - 1)].ToString());
+                }
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
